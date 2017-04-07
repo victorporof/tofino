victorporof/tofino

commit 3155ca5fdce161bf68e5532fd414d7b88afbfe6b

Add support for webpack dev server in development builds
Signed-off-by: Victor Porof <victor.porof@gmail.com>

build/tasks/task-browser-frontend.js
@@ -5,11 +5,11 @@ import gulp from 'gulp';
 import debug from 'gulp-debug';
 import changed from 'gulp-changed';
 import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
 import colors from 'colors/safe';
 
-import logger from '../logger';
-
 import * as Paths from '../../src/shared/paths';
+import * as Endpoints from '../../src/shared/endpoints';
 import config from '../webpack/config.browser-frontend';
 import { makeDevConfig, makeProdConfig } from '../webpack/config.base';
 
@@ -25,17 +25,20 @@ gulp.task('browser-frontend:copy-html', () =>
     .pipe(gulp.dest(Paths.BROWSER_FRONTEND_DST)),
 );
 
-gulp.task('browser-frontend:webpack', () => new Promise((resolve, reject) => {
+gulp.task('browser-frontend:webpack', (cb) => {
   const currentConfig = process.env.NODE_ENV === 'production'
     ? makeProdConfig(config)
     : makeDevConfig(config);
 
-  webpack(currentConfig, (err, stats) => {
-    if (err) { reject(err); return; }
-    if (stats) { logger.log(stats.toString(WEBPACK_STATS_OPTIONS)); }
-    resolve();
+  const compiler = webpack(currentConfig);
+  const server = new WebpackDevServer(compiler, {
+    stats: WEBPACK_STATS_OPTIONS,
   });
-}));
+
+  server.listen(
+    Endpoints.WEBPACK_DEV_SERVER_PORT,
+    Endpoints.WEBPACK_DEV_SERVER_HOST, cb);
+});
 
 gulp.task('browser-frontend:build', gulp.series(
   'browser-frontend:copy-html',

src/browser-server/routes/chrome.js
@@ -11,11 +11,23 @@ specific language governing permissions and limitations under the License.
 */
 
 import express from 'express';
+import proxy from 'express-http-proxy';
 
 import * as Paths from '../../shared/paths';
+import * as Endpints from '../../shared/endpoints';
 
 export default ({ pathname, app }) => {
   const router = express.Router();
   app.use(pathname, router);
+
+  // In development builds, we want to reroute script bundle requests to the
+  // webpack dev server, which incrementally rebuilds the browser chrome,
+  // in memory, when sources change.
+  if (process.env.NODE_ENV === 'development') {
+    const frontendBundle = Paths.BROWSER_FRONTEND_BUNDLED_FILENAME;
+    const webpackServer = `${Endpints.WEBPACK_DEV_SERVER_HOST}:${Endpints.WEBPACK_DEV_SERVER_PORT}`;
+    router.get(`*${frontendBundle}`, proxy(webpackServer));
+  }
+
   router.use(express.static(Paths.BROWSER_FRONTEND_DST));
 };

src/shared/endpoints.js
@@ -1,0 +1,5 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+export const WEBPACK_DEV_SERVER_HOST = 'localhost';
+export const WEBPACK_DEV_SERVER_PORT = 8990;

commit dc5516486efe6a1b63f3bcb521982f4cd075a55a

Fix regression caused by 3155ca5 where `npm start` would be broken
The trouble was that we where using webpack-dev-server in production builds as well, while our browser server was configured to proxy requests only in development mode

Signed-off-by: Victor Porof <victor.porof@gmail.com>

build/tasks/task-browser-frontend.js
@@ -8,6 +8,8 @@ import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import colors from 'colors/safe';
 
+import logger from '../logger';
+
 import * as Paths from '../../src/shared/paths';
 import * as Endpoints from '../../src/shared/endpoints';
 import config from '../webpack/config.browser-frontend';
@@ -27,30 +29,43 @@ gulp.task('browser-frontend:copy-html', () =>
     .pipe(gulp.dest(Paths.BROWSER_FRONTEND_DST)),
 );
 
-gulp.task('browser-frontend:webpack', (cb) => {
-  const currentConfig = process.env.NODE_ENV === 'production'
-    ? makeProdConfig(config)
-    : makeDevConfig(config);
+gulp.task('browser-frontend:webpack', () => new Promise((resolve, reject) => {
+  if (process.env.NODE_ENV !== 'development') {
+    // In a production environment, webpack directly to disk.
+    const currentConfig = makeProdConfig(config);
+    webpack(currentConfig, (err, stats) => {
+      if (err) { reject(err); return; }
+      if (stats) { logger.log(stats.toString(WEBPACK_STATS_OPTIONS)); }
+      resolve();
+    });
+  } else {
+    // In a development environment, webpack in memory and serve
+    // the build with webpack-dev-server.
+    const currentConfig = makeDevConfig(config);
+    const compiler = webpack(currentConfig);
+    const server = new WebpackDevServer(compiler, {
+      stats: WEBPACK_STATS_OPTIONS,
+    });
 
-  const compiler = webpack(currentConfig);
-  const server = new WebpackDevServer(compiler, {
-    stats: WEBPACK_STATS_OPTIONS,
-  });
+    // We'll have to close the webpack dev server later, so store it as a global.
+    // If we leave the server open, the build process will keep running forever.
+    gWebpackDevServer = server;
 
-  // We'll have to close the webpack dev server later, so store it as a global.
-  // If we leave the server open, the build process will keep running forever.
-  gWebpackDevServer = server;
-
-  server.listen(
-    Endpoints.WEBPACK_DEV_SERVER_PORT,
-    Endpoints.WEBPACK_DEV_SERVER_HOST, cb);
-});
+    server.listen(
+      Endpoints.WEBPACK_DEV_SERVER_PORT,
+      Endpoints.WEBPACK_DEV_SERVER_HOST, resolve);
+  }
+}));
 
 gulp.task('browser-frontend:build', gulp.series(
   'browser-frontend:copy-html',
   'browser-frontend:webpack',
 ));
 
-gulp.task('browser-frontend:build:cleanup', cb =>
-  gWebpackDevServer.close(cb),
-);
+gulp.task('browser-frontend:build:cleanup', (cb) => {
+  if (process.env.NODE_ENV === 'development') {
+    gWebpackDevServer.close(cb);
+  } else {
+    cb();
+  }
+});
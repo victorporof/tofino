victorporof/tofino

commit 9a1fb40ac866cc1a9259a7f2ea59380e66a8ef56

First implementation of mozbrowser api on top of qbrt (#9)

src/shared/paths.js
@@ -88,6 +88,9 @@ export const QBRT_RUNNER_ENTRY_FILENAME = 'platform-qbrt/index.js';
 export const QBRT_RUNNER_POLYFILL_FILENAME = 'browser-runner-qbrt-polyfill.js';
 export const QBRT_RUNNER_DST_MAIN = path.join(BROWSER_RUNNER_DST, QBRT_RUNNER_POLYFILL_FILENAME);
 
+export const QBRT_RUNNER_SHELL_SRC = path.join(BROWSER_RUNNER_SRC, 'platform-qbrt', 'shell');
+export const QBRT_RUNNER_SHELL_DST = path.join(BROWSER_RUNNER_DST, 'platform-qbrt', 'shell');
+
 /**
  * Browser server paths.
  */

.eslintrc
@@ -16,7 +16,10 @@
   ],
 
   "rules": {
-    "max-len": ["error", 120],
+    "max-len": ["error", {
+      "code": 120,
+      "ignoreUrls": true
+    }],
     // Allow underscore because we're using it to signal private class fields,
     // enforced by the babel plugin `transform-private-underscore`.
     "no-underscore-dangle": ["off"],

build/tasks/task-browser-frontend.js
@@ -22,12 +22,18 @@ const WEBPACK_STATS_OPTIONS = {
 
 let gWebpackDevServer;
 
-gulp.task('browser-frontend:copy-html', () =>
-  gulp.src(`${Paths.BROWSER_FRONTEND_SRC}/**/*.html`)
+gulp.task('browser-frontend:copy-html', () => {
+  return gulp.src(`${Paths.BROWSER_FRONTEND_SRC}/**/*.html`)
     .pipe(changed(Paths.BROWSER_FRONTEND_DST))
     .pipe(debug({ title: `Running ${colors.cyan('cp')}` }))
-    .pipe(gulp.dest(Paths.BROWSER_FRONTEND_DST)),
-);
+    .pipe(gulp.dest(Paths.BROWSER_FRONTEND_DST));
+});
+
+gulp.task('browser-frontend:copy-to-qbrt-shell', () => {
+  return gulp.src(`${Paths.BROWSER_FRONTEND_DST}/**/*`)
+    .pipe(changed(Paths.QBRT_RUNNER_SHELL_DST))
+    .pipe(gulp.dest(Paths.QBRT_RUNNER_SHELL_DST));
+});
 
 gulp.task('browser-frontend:webpack', () => new Promise((resolve, reject) => {
   if (process.env.NODE_ENV !== 'development') {
@@ -60,6 +66,7 @@ gulp.task('browser-frontend:webpack', () => new Promise((resolve, reject) => {
 gulp.task('browser-frontend:build', gulp.series(
   'browser-frontend:copy-html',
   'browser-frontend:webpack',
+  'browser-frontend:copy-to-qbrt-shell',
 ));
 
 gulp.task('browser-frontend:build:cleanup', (cb) => {

build/tasks/task-browser-runner.js
@@ -60,9 +60,16 @@ gulp.task('qbrt-runner:polyfill', () => {
     .pipe(gulp.dest(Paths.BROWSER_RUNNER_DST));
 });
 
+gulp.task('qbrt-runner:shell', () => {
+  return gulp.src(`${Paths.QBRT_RUNNER_SHELL_SRC}/**/*`)
+    .pipe(changed(Paths.QBRT_RUNNER_SHELL_DST))
+    .pipe(gulp.dest(Paths.QBRT_RUNNER_SHELL_DST));
+});
+
 gulp.task('browser-runner:build', gulp.series(
   'browser-runner:babel',
   'electron-runner:polyfill',
   'dummy-runner:polyfill',
   'qbrt-runner:polyfill',
+  'qbrt-runner:shell',
 ));

src/browser-frontend/constants/endpoints.js
@@ -12,9 +12,13 @@ specific language governing permissions and limitations under the License.
 
 import url from 'url';
 
-export const SERVER_API_VERSION = location.pathname.match(/\/(v[0-9]+)\//).pop();
-export const SERVER_HOSTNAME = location.hostname;
-export const SERVER_PORT = location.port;
+// Support loading a file over chrome URI for qbrt by allowing these to be passed
+// as search params
+const params = new URLSearchParams(location.search);
+export const SERVER_API_VERSION = params.get('apiVersion') ||
+                                  location.pathname.match(/\/(v[0-9]+)\//).pop();
+export const SERVER_HOSTNAME = params.get('hostname') || location.hostname;
+export const SERVER_PORT = params.get('port') || location.port;
 
 export const SERVER_WS_ROUTE = url.format({
   protocol: 'ws:',

src/browser-runner/platform-qbrt/sagas/server-command-handlers.js
@@ -10,19 +10,41 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 */
 
+import { parse } from 'url';
+import querystring from 'querystring';
+
 import { takeEvery, call } from 'redux-saga/effects';
 import { spawn } from '../../../shared/util/spawn';
-
+import * as Paths from '../../../shared/paths';
 import logger from '../../logger';
-
 import SharedActions from '../../../shared/actions/shared-actions';
 
-function* create({ meta: client, payload: { winId, url } }) {
+function* create({ meta: client, payload: { winId, url, width, height, style } }) {
   logger.log(`Chrome frontend hosted at ${url}`);
   const qbrt = process.platform === 'win32'
     ? 'qbrt.cmd'
     : 'qbrt';
-  spawn(qbrt, 'run', [url], { logger });
+
+  // Normally these parameters are inferred from the url in the frontend, but when
+  // running in qbrt the frontend is loaded as a chrome:// url so we need to tack
+  // these on as get parameters
+  const parsedURL = parse(url);
+  const extraParams = querystring.stringify({
+    port: parsedURL.port,
+    hostname: parsedURL.hostname,
+    apiVersion: parsedURL.pathname.split('/')[1],
+  });
+  const searchParams = `${parsedURL.search}&${extraParams}`;
+  const args = [Paths.QBRT_RUNNER_SHELL_DST,
+    '--width', width,
+    '--height', height,
+    '--style', style,
+    '--searchParams', searchParams,
+  ];
+
+  // TODO: Pass important params (server version, hostname, port, etc) along as
+  // args once https://github.com/mozilla/qbrt/pull/51 lands
+  spawn(qbrt, 'run', args, { logger });
   yield call([client, client.send], SharedActions.events.fromRunner.toServer.app.window.created({ winId }));
 }
 

src/browser-runner/platform-qbrt/shell/main.js
@@ -1,0 +1,63 @@
+/* Copyright 2017 Mozilla
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
+/* global Components, dump */
+
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+const { Runtime } = Cu.import('resource://qbrt/modules/Runtime.jsm', {});
+const { Services } = Cu.import('resource://gre/modules/Services.jsm', {});
+
+// Slurp up command line args passed into qbrt and convert them to GET params
+// for the frontend.
+
+let width = 640;
+let height = 480;
+let searchParams = null;
+for (let i = 1; i < Runtime.commandLineArgs.length; i += 2) {
+  if (Runtime.commandLineArgs[i].indexOf('-width') !== -1) {
+    width = Runtime.commandLineArgs[i + 1];
+  } else if (Runtime.commandLineArgs[i].indexOf('-height') !== -1) {
+    height = Runtime.commandLineArgs[i + 1];
+  } else if (Runtime.commandLineArgs[i].indexOf('-searchParams') !== -1) {
+    searchParams = Runtime.commandLineArgs[i + 1];
+  }
+}
+
+const SHELL_URL = `chrome://app/content/index.html${searchParams}`;
+
+dump(`Loading frontend at: ${SHELL_URL}\n`);
+
+// TODO: Handle `style` argument for window chrome
+const WINDOW_FEATURES = [
+  `width=${width}`,
+  `height=${height}`,
+  'resizable',
+  'scrollbars',
+].join(',');
+
+// On startup, activate ourselves, since starting up from Node doesn't do this.
+// TODO: do this by default for all apps started via Node.
+if (Services.appinfo.OS === 'Darwin') {
+  Cc['@mozilla.org/widget/macdocksupport;1'].getService(Ci.nsIMacDockSupport).activateApplication(true);
+}
+
+// TODO: Find a way to pass the correct browser-frontend URL down to this process
+const url = Runtime.commandLineArgs[0] || Runtime.packageJSON.mainURL || 'index.html';
+const argument = Cc['@mozilla.org/supports-string;1'].createInstance(Ci.nsISupportsString);
+argument.data = url;
+
+// TODO: Set up 'hot reload' by using incremental builds and taking over ctrl+r
+
+const win = Services.ww.openWindow(null, SHELL_URL, '_blank', WINDOW_FEATURES, argument);
+Runtime.openDevTools(win);

src/shared/widgets/web-contents/iframe-mozbrowser.jsx
@@ -14,18 +14,30 @@ import React, { PureComponent } from 'react';
 
 import WebContents from '.';
 
+// Implement a mozbrowser, see following docs and examples:
+// https://developer.mozilla.org/en-US/docs/Web/API/Using_the_Browser_API
+// https://github.com/mdn/browser-api-demo/blob/master/main.js
+// https://github.com/mozilla/positron-electron/blob/3345aa05f8d55e0c91abbf52489b6c7e40a336d5/lib/renderer/web-view/web-view.js#L300-L360
 export default class IframeMozBrowser extends PureComponent {
   constructor(...args) { // eslint-disable-line
     super(...args);
   }
 
   componentDidMount() {
+    this._iframe.addEventListener('mozbrowsertitlechange', (e) => {
+      this.props.onPageTitleSet({ title: e.detail });
+    });
+    this._iframe.addEventListener('mozbrowserlocationchange', (e) => {
+      this.props.onDidNavigate({ url: e.detail.url });
+    });
     this._iframe.addEventListener('mozbrowserloadstart', () => {
       this.props.onDidStartLoading();
     });
     this._iframe.addEventListener('mozbrowserloadend', () => {
       this.props.onDidStopLoading();
-      this.props.onPageTitleSet({ title: 'Loaded' });
+    });
+    this._iframe.addEventListener('mozbrowsererror', () => {
+      this.props.onDidFailLoad();
     });
   }
 
@@ -50,7 +62,7 @@ export default class IframeMozBrowser extends PureComponent {
   }
 
   reload = () => {
-    throw new Error('Not implemented.');
+    this._iframe.reload();
   }
 
   render() {

commit cc42131fb98bff7b9e96ba97d455168d45be9993

Fix regression caused by 9a1fb40 where packaged builds wouldn't start anymore
Signed-off-by: Victor Porof <victor.porof@gmail.com>

src/shared/paths.js
@@ -85,8 +85,9 @@ export const DUMMY_RUNNER_DST_MAIN = path.join(BROWSER_RUNNER_DST, DUMMY_RUNNER_
 export const QBRT_RUNNER_ENTRY_FILENAME = 'platform-qbrt/index.js';
 export const QBRT_RUNNER_DST_MAIN = path.join(BROWSER_RUNNER_DST, QBRT_RUNNER_ENTRY_FILENAME);
 
-export const QBRT_RUNNER_SHELL_SRC = path.join(BROWSER_RUNNER_SRC, 'platform-qbrt', 'shell');
-export const QBRT_RUNNER_SHELL_DST = path.join(BROWSER_RUNNER_DST, 'platform-qbrt', 'shell');
+export const QBRT_SHELL_DIRNAME = 'platform-qbrt/shell';
+export const QBRT_RUNNER_SHELL_SRC = IS_PACKAGED_BUILD ? undefined : path.join(BROWSER_RUNNER_SRC, QBRT_SHELL_DIRNAME);
+export const QBRT_RUNNER_SHELL_DST = path.join(BROWSER_RUNNER_DST, QBRT_SHELL_DIRNAME);
 
 /**
  * Browser server paths.